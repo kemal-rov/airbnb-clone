049ad14b010e60b1688cf5d5d1a87c691289f322
Kemal R <kemo996@gmail.com>
2024-04-15 16:14:48 +0200

    install tailwindcss

--- airbnb-clone/src/app/layout.tsx
+++ airbnb-clone/src/app/layout.tsx
@@ -1,10 +1,7 @@
 import { Nunito } from 'next/font/google'
-
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Airbnb Clone",
@@ -22,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
