6e7c02311f9744ac712a448b26530a0dc40e3784
Kemal R <kemo996@gmail.com>
2024-04-16 13:28:27 +0200

    build modal component for Auth UI

--- airbnb-clone/src/app/layout.tsx
+++ airbnb-clone/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Nunito } from 'next/font/google';
 import Navbar from './components/navbar/Navbar';
+import Modal from './components/modals/Modal'
+import ClientOnly from './components/ClientOnly';
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -21,7 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar></Navbar>
+        <ClientOnly>
+          <Modal title="Helloooo" isOpen />
+          <Navbar />
+        </ClientOnly>
         {children}</body>
     </html>
   );
