fce8f4dcf553e9f384f8c16ef0d43785e991f84e
Kemal R <kemo996@gmail.com>
2024-04-17 14:02:32 +0200

    fix next auth

--- airbnb-clone/src/app/layout.tsx
+++ airbnb-clone/src/app/layout.tsx
@@ -2,6 +2,8 @@ import { Nunito } from 'next/font/google';
 import ToasterProvider from '@/providers/ToasterProvider';
 import Navbar from './components/navbar/Navbar';
 import RegisterModal from './components/modals/RegisterModal';
+import LoginModal from './components/modals/LoginModal';
+
 import ClientOnly from './components/ClientOnly';
 import type { Metadata } from "next";
 import "./globals.css";
@@ -26,6 +28,7 @@ export default function RootLayout({
         <ClientOnly>
           <ToasterProvider />
           <RegisterModal />
+          <LoginModal />
           <Navbar />
         </ClientOnly>
         {children}</body>
