08c1647eef7eb9e8c23ccb2dc2ccaf59676ee320
Kemal R <kemo996@gmail.com>
2024-04-23 07:54:47 +0200

    use custom hook for world countries

--- airbnb-clone/src/app/actions/getCurrentUser.ts
+++ airbnb-clone/src/app/actions/getCurrentUser.ts
@@ -23,6 +23,7 @@ export default async function getCurrentUser() {
         if (!currentUser) {
             return null;
         }
+        console.log(currentUser)
 
         return {
             ...currentUser,
